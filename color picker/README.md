fd77f8f95bff4b6864ce87f558830b1c9ef2bed7
Abhishek_ub <ubabhishek@gmail.com>
2025-08-14 23:20:48 +0530

    Add Color Picker project with HTML, CSS, JS and README

---
+++ color picker/README.md
@@ -0,0 +1,3 @@
+# Color Picker Project
+
+A mini project using HTML, CSS, and JavaScript to create a color picker app.
---
+++ color picker/script.js
@@ -0,0 +1,22 @@
+let ui_color = document.getElementById("colorPickerContainer");
+let card_text = document.getElementById("selectedColorHexCode");
+
+function button_1() {
+    ui_color.style.backgroundColor = "#e0e0e0";
+    card_text.textContent = "#e0e0e0";
+}
+
+function button_2() {
+    ui_color.style.backgroundColor = "#6fcf97";
+    card_text.textContent = "#6fcf97";
+}
+
+function button_3() {
+    ui_color.style.backgroundColor = "#56ccf2";
+    card_text.textContent = "#56ccf2";
+}
+
+function button_4() {
+    ui_color.style.backgroundColor = "#bb6bd9";
+    card_text.textContent = "#bb6bd9";
+}
